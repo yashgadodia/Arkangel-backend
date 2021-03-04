f62690cc17cb764d6e99eef243c64a29c6bd2884
Bing Huang <bing2609@gmail.com>
2021-03-04 19:58:10 +0800

    useCases: trackUser: updated bot to send next question

--- useCases/trackUser/index.js
+++ useCases/trackUser/index.js
@@ -1,12 +1,31 @@
 const bot = require('../../lib/TelegramBot')
-const question = ""
+const question = "Please share with us your live location with us to estimate your ETA to your destination."
 
 module.exports = {
     question,
-
-
+    
     handler: function (message) {
         const chatId = message.chat.id
-        const userId = message.from.id
+
+        bot.sendMessage(chatId, question, {
+            reply_markup: {
+                force_reply: true
+            }
+        })
+
+        // bot.sendMessage(chatId, question, {
+        //     reply_markup: {
+        //         inline_keyboard: [
+        //             [
+        //                 {
+        //                     text: ''
+        //                 },
+        //                 {
+        //                     text: ''
+        //                 }
+        //             ]
+        //         ]
+        //     }
+        // })
     }
 }
