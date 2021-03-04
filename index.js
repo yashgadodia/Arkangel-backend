2c5e067a2cdd1785b0fbb2bb0a5d59a9ebf2fcb0
Bing Huang <bing2609@gmail.com>
2021-03-04 13:23:43 +0800

    useCases: onboardUser: added /start command for telegram

--- index.js
+++ index.js
@@ -1,4 +1,9 @@
 require('dotenv').config()
 const bot = require('./lib/TelegramBot')
 
+// TODO: setup webhooks for Telegram bot
+
 // Telegram bot setup
+bot.onText(/\/start/, require('./useCases/onboardUser'))
+
+// JSON RPC setup
