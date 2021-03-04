664d6ed75017acf068ae0ec2751cdc4b0da8273d
Bing Huang <bing2609@gmail.com>
2021-03-04 19:57:05 +0800

    model: TrackUserInputModel: initial implementation

---
+++ model/TrackUserInputModel.js
@@ -0,0 +1,13 @@
+module.exports = () => ({
+    chatId: null,
+    currentLocation: {
+        lat: null,
+        lng: null
+    },
+    destinationName: null,
+    destinationLocation: {
+        lat: null,
+        lng: null
+    },
+    etaToDestination: null
+})
