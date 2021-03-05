db3651196184431c7972b3aaea06b152f113ed01
Bing Huang <bing2609@gmail.com>
2021-03-05 18:25:44 +0800

    useCases: onboardUser: index: ensure only one user is created

--- useCases/onboardUser/index.js
+++ useCases/onboardUser/index.js
@@ -1,9 +1,16 @@
+const firebaseDb = require('../../lib/FireBaseDB')
 const bot = require('../../lib/TelegramBot')
 const nameListener = require('./nameListener')
 
-module.exports = function (message) {
+module.exports = async function (message) {
     const chatId = message.chat.id
 
-    bot.sendMessage(chatId, 'Hello and welcome to ArkAngel, let\'s get you started by providing your information for us to keep record.')
-    nameListener.prompt(chatId)
+    bot.sendChatAction(chatId, 'typing')
+    const isUserCreated = await firebaseDb.isUserExist(message.chat.username)
+    if (isUserCreated) {
+        bot.sendMessage(chatId, 'Thank you for registering with ArkAngel.')
+    } else {
+        bot.sendMessage(chatId, 'Hello and welcome to ArkAngel, let\'s get you started by providing your information for us to keep record.')
+        nameListener.prompt(chatId)
+    }
 }
