cb98f4c06ec52a2f02fe0154e2fb8a63c522a506
Bing Huang <bing2609@gmail.com>
2021-03-05 16:23:27 +0800

    useCases: common: callbackQueryListener: redirect to eta handlers

--- useCases/common/callbackQueryListener.js
+++ useCases/common/callbackQueryListener.js
@@ -1,3 +1,22 @@
+const bot = require('../../lib/TelegramBot')
+const localTrackingContext = require('../../lib/LocalTrackingContext')
+
+const etaListener = require('../trackUser/etaListener')
+
 module.exports = (message) => {
-    console.log(message)
+    const tracker = localTrackingContext.getTracker(message.from.id)
+    if (!tracker) return
+
+    bot.editMessageReplyMarkup({ inline_keyboard: [[]] }, { chat_id: tracker.chatId, message_id: message.message.message_id })
+    bot.answerCallbackQuery(message.id)
+
+    switch (message.data) {
+        case 'etaListener_auto':
+            etaListener.autoHandler(message)
+            break
+
+        case 'etaListener_manual':
+            etaListener.promptManualEta(message.message.chat.id)
+            break
+    }
 }
