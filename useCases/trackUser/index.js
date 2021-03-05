1ba48b54cbf8b8a55c624e3a1e01b59876297958
Bing Huang <bing2609@gmail.com>
2021-03-05 19:57:23 +0800

    useCases: trackUser: prevent multiple tracker to be activated

--- useCases/trackUser/index.js
+++ useCases/trackUser/index.js
@@ -1,6 +1,15 @@
+const bot = require('../../lib/TelegramBot')
+const localTrackingContext = require('../../localContext/LocalTrackingContext')
 const currentLocationListener = require('./currentLocationListener')
 
 module.exports = message => {
     const chatId = message.chat.id
-    currentLocationListener.prompt(chatId)
+    const userId = message.from.id
+
+    const tracker = localTrackingContext.getTracker(userId)
+    if (tracker && tracker.shouldPerformHealthCheck) {
+        bot.sendMessage(chatId, "We have already started tracking you")
+    } else {
+        currentLocationListener.prompt(chatId)
+    }
 }
