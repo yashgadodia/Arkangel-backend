91d899178a4baeec38becdaa185ef85acefbad36
Bing Huang <bing2609@gmail.com>
2021-03-21 12:58:32 +0800

    index: added rpc/backend listener to send telegram messages

--- index.js
+++ index.js
@@ -1,4 +1,5 @@
 require('dotenv').config()
+const { database: FirebaseDb, getUserByTeleHandle } = require('./lib/FireBaseDB')
 const bot = require('./lib/TelegramBot')
 
 // TODO: setup webhooks for Telegram bot
@@ -9,3 +10,26 @@ bot.onText(/\/sendhelp/, require('./useCases/sendHelp').handler)
 
 bot.on('message', require('./useCases/common/messageListener'))
 bot.on('callback_query', require('./useCases/common/callbackQueryListener'))
+
+const rpcListener = (function () {
+    const backendRpcObserver = FirebaseDb.ref('rpc/backend')
+    backendRpcObserver.on('child_added', snapshot => {
+        const { method, payload } = snapshot.toJSON()
+
+        switch (method) {
+            case 'sendUser':
+                sendUserMessage(snapshot.key, payload)
+                break
+        }
+
+    })
+}())
+
+async function sendUserMessage(fbRowKey, { teleHandle, message }) {
+    const user = await getUserByTeleHandle(teleHandle)
+    if (!user) return
+
+    const { teleChatId } = user
+    await bot.sendMessage(teleChatId, message)
+    await FirebaseDb.ref(`rpc/backend/${fbRowKey}`).remove()
+}
